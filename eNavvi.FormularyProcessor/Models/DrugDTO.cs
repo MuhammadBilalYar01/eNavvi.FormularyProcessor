58e4629306443e5f20c8dade2845e545f7cb1175
Muhammad.Bilal <Muhammad.Bilal.ctr@enavvi.com>
2022-02-02 13:24:49 +0500

    Added one new model

---
+++ eNavvi.FormularyProcessor/Models/DrugDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eNavvi.FormularyProcessor.Models
+{
+    public class DrugDTO
+    {
+        public int DrugId { get; set; }
+        public bool IsQuantityLimited { get; set; }
+        public bool IsStepTherapy { get; set; }
+        public bool IsPriorAuthorization { get; set; }
+        public string DrugName { get; set; }
+        public int? Rxcui { get; set; }
+        public string PlanName { get; set; }
+        public int PlanId { get; set; }
+    }
+}
